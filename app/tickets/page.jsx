e58f17ca36b2356bb54483be374e2459a17b34cc
iltioy <tema.illar2@gmail.com>
2023-12-01 22:20:46 +0300

    final

---
+++ app/tickets/page.jsx
@@ -0,0 +1,24 @@
+import React, { Suspense } from "react";
+import TicketList from "../components/TicketList";
+import Loading from "./loading";
+
+const Tickets = () => {
+    return (
+        <main>
+            <nav>
+                <div>
+                    <h2>Tickets</h2>
+                    <p>
+                        <small>Currently open tickets.</small>
+                    </p>
+                </div>
+            </nav>
+
+            <Suspense fallback={<Loading />}>
+                <TicketList />
+            </Suspense>
+        </main>
+    );
+};
+
+export default Tickets;
